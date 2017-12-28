74d24717474e6826039e0cf8d5bf530b57010989
samalws <saalws@ctemc.org>
2017-12-28 14:22:36 -0500

    eth style transaction

--- chain/eth_style_txn.cpp
+++ chain/eth_style_txn.cpp
@@ -1,8 +1,14 @@
+#include "byteable.cpp"
+#include "code.cpp"
+
 #ifndef ETH_STYLE_TXN_CPP
 #define ETH_STYLE_TXN_CPP
 
 namespace chain {
-  struct EthStyleTxn {};
+  struct EthStyleTxn: public Byteable {
+    CodeTxn codeTxn;
+    CodeResults *results;
+  };
 }
 
 #endif
